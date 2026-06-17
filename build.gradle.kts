8e410d9c70b703b6b78e1864f3b504445187c5d1
Egil Ballestad <egil.ballestad@vigoiks.no>
2026-06-17 14:37:20 +0200

    bump to java 25 and bump web-instance-gateway

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot") version "3.5.9"
+    id("org.springframework.boot") version "3.5.15"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.4.0"
     kotlin("plugin.spring") version "2.4.0"
