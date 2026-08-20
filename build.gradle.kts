bbf8ac9dd83e2d21e782c0233695eb685419d7a7
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-20 14:29:06 +0200

    fix: overstyr httpcore5 og httpclient5

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,8 @@ repositories {
     mavenLocal()
 }
 
+extra["httpclient5.version"] = "5.6.3"
+extra["httpcore5.version"] = "5.4.3"
 extra["jackson-bom.version"] = "2.21.5"
 extra["log4j2.version"] = "2.25.5"
 
