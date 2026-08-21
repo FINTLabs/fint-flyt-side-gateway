a4c0f2867284100949ae2f4100dce2b86de2deb7
Jan Ove Kongshaug <jan.ove.kongshaug@novari.no>
2026-08-21 10:19:55 +0200

    FFS-2278 Overstyr sårbare avhengighetsversjoner (#22)
    * fix: overstyr sårbare avhengighetsversjoner
    
    * fix: kjør Dependabot på mandag og legg til CODEOWNERS
    
    * fix: overstyr httpcore5 og httpclient5
    
    * fix: pin lz4-java og commons-compress til patchede versjoner
    
    * fix: bump lz4-java til 1.11.2
    
    * fix: bump jackson, log4j2, netty og commons-compress

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,18 @@ repositories {
     mavenLocal()
 }
 
+extra["httpclient5.version"] = "5.6.3"
+extra["httpcore5.version"] = "5.4.3"
+extra["jackson-bom.version"] = "2.22.2"
+extra["log4j2.version"] = "2.26.1"
+
 dependencies {
+    constraints {
+        implementation("at.yawk.lz4:lz4-java:1.11.2") {
+            because("Fixes CVE-2026-59949 in the kafka-clients transitive dependency")
+        }
+    }
+
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
